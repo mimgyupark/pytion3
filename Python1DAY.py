e66042f2fa285b8c58b1728a6a3afafefc36f63c
Park MinGyu <ppp1532@naver.com>
2020-02-04 14:51:47 +0900

    Python first class

---
+++ Python1DAY.py
@@ -0,0 +1,23 @@
+git init
+git add pytion공부
+x=2
+x=x+2
+print(x)
+print("_________________________________")
+X=2
+print(x)
+print("__________________________________")
+x=x+2
+print(x)
+x=5
+if x<10: 
+   print('small')
+if x>20:
+   print('bigger')
+print('finis')
+print('____________________')
+n=5
+while n>0:
+    print(n)
+    n=n-1
+print('blastoff')
