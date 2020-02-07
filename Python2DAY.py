b901591a59b2aa76c842b3073d583cb171b3ba08
Park MinGyu <ppp1532@naver.com>
2020-02-07 14:23:05 +0900

    pytion second class

---
+++ Python2DAY.py
@@ -0,0 +1,11 @@
+#박민규pythionDAY2
+print(10/2)
+print('_______________')
+x=1+2**3/4*5
+print(x)
+print('_______________')
+print(float(99)+100)
+print('_______________')
+#임금계산기
+hours=float(input('how many hours you worked?'))
+print('your pay is',hours*2.75,'dollar')
